6ba8be6f6e93a7ce238a8778cf7858d269d45eba
oshadaera68 <mindartlk010@gmail.com>
2024-04-05 14:25:53 +0530

    updated methods

--- tracker.py
+++ tracker.py
@@ -1,14 +1,29 @@
 # finance tracker v2.0: using dictionaries + json
+# importing json module
+import json
 
-# file handling functions
-# load all datas in the json file
+# initialize the empty directory
+transactions = []
+
+
+# file handling
+# load the file
 def load_transactions():
-    pass
+    global transactions
+    try:
+        with open("trans.json", "r") as file:
+            json.load(file)
+    except FileNotFoundError:
+        print("No transactions. Please try again")
+    except json.decoder.JSONDecodeError:
+        transactions = []
 
 
 # save the transactions
 def save_transactions():
-    pass
+    # write the data in the json file
+    with open("trans.json", "w") as file:
+        json.dump(transactions, file)
 
 
 def read_bulk_transactions_from_file(filename):
@@ -18,7 +33,13 @@ def read_bulk_transactions_from_file(filename):
 
 # Feature implementations
 def add_transaction():
-    pass
+    enter_choice = input("Transaction Completed. Do you want to add the another Transaction? [Y/N]:")
+    if enter_choice == "y" or enter_choice == "Y":
+        add_transaction()
+    elif enter_choice == "n" or enter_choice == "N":
+        main_menu()
+    else:
+        print("Invalid Value. Please Try Again!!")
 
 
 def view_transactions():
@@ -26,11 +47,23 @@ def view_transactions():
 
 
 def update_transaction():
-    pass
+    enter_choice = input("Transaction Completed. Do you want to add the another Transaction? [Y/N]:")
+    if enter_choice == "y" or enter_choice == "Y":
+        add_transaction()
+    elif enter_choice == "n" or enter_choice == "N":
+        main_menu()
+    else:
+        print("Invalid Value. Please Try Again!!")
 
 
 def delete_transaction():
-    pass
+    enter_choice = input("Transaction Completed. Do you want to add the another Transaction? [Y/N]:")
+    if enter_choice == "y" or enter_choice == "Y":
+        add_transaction()
+    elif enter_choice == "n" or enter_choice == "N":
+        main_menu()
+    else:
+        print("Invalid Value. Please Try Again!!")
 
 
 def display_summary():
@@ -38,22 +71,6 @@ def display_summary():
 
 
 def main_menu():
-    pass
-
-
-def exit_the_program():
-    exit_choice = input("Did you want to exit the System? [Y/N]: ")
-    if exit_choice == "y" or exit_choice == "Y":
-        print("Program Exited")
-        exit()
-    elif exit_choice == "n" or exit_choice == "N":
-        main_menu()
-    else:
-        print("Invalid Input.")
-        exit(0)
-
-
-if __name__ == "__main__":
     # load all transactions in the json file
     load_transactions()
     while True:
@@ -82,3 +99,20 @@ if __name__ == "__main__":
             exit_the_program()
         else:
             print("Invalid choice. Please try again.")
+
+
+def exit_the_program():
+    exit_choice = input("Did you want to exit the System? [Y/N]: ")
+    if exit_choice == "y" or exit_choice == "Y":
+        print("Program Exited")
+        exit()
+    elif exit_choice == "n" or exit_choice == "N":
+        main_menu()
+    else:
+        print("Invalid Input.")
+        exit(0)
+
+
+# runnable main constructor
+if __name__ == "__main__":
+    main_menu()
